8b9e0ac4ad1bfa275e52bbebff48c4e5a9174a4e
Ilya96s <i.kaltygin@icloud.com>
2024-02-18 22:35:41 +0700

    created model License

---
+++ src/main/java/com/optimagowth/license/model/License.java
@@ -0,0 +1,25 @@
+package com.optimagowth.license.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class License {
+
+    private int id;
+
+    private String licenseId;
+
+    private String description;
+
+    private String organizationId;
+
+    private String productName;
+
+    private String licenseType;
+}
