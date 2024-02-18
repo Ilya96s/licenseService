e69e6e34e0ebff4c4098dce6c4ebe5deae974596
Ilya96s <i.kaltygin@icloud.com>
2024-02-18 21:17:51 +0700

    init commit

---
+++ src/test/java/com/optimagowth/license/LicenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagowth.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
