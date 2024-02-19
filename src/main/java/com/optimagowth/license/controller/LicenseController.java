8bc02ac36085e693bbf941f474dfc2d5d752ea7a
Ilya96s <i.kaltygin@icloud.com>
2024-02-19 14:31:29 +0700

    Implemented Spring HATEOAS to display related links

--- src/main/java/com/optimagowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagowth/license/controller/LicenseController.java
@@ -3,10 +3,13 @@ package com.optimagowth.license.controller;
 import com.optimagowth.license.model.License;
 import com.optimagowth.license.service.LicenseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping("v1/organization/{organizationId}/license")
@@ -19,6 +22,19 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createlicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
